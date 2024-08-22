d6e2251efd56360ed5ff26319cba8b1c72c609b4
jsopn <me@jsopn.com>
2024-08-22 12:59:56 +0400

    flake: add deploy-rs with alpha config for it

--- flake.nix
+++ flake.nix
@@ -4,12 +4,13 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
 
+    deploy-rs.url = "github:serokell/deploy-rs";
     nur.url = "github:nix-community/NUR";
     agenix.url = "github:ryantm/agenix";
     stylix.url = "github:danth/stylix";
   };
 
-  outputs = { self, ... } @ inputs:
+  outputs = { self, deploy-rs, ... } @ inputs:
     let
       inherit (self) outputs;
 
@@ -26,5 +27,23 @@
       
       nixosModules = modules.nixos;
       homeManagerModules = modules.hm;
+
+      # -- 
+
+      deploy.nodes.alpha = {
+        hostname = "10.100.20.100";
+        interactiveSudo = true;
+
+        profiles.system = {
+          user = "root";
+          sshUser = "jsopn";
+
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.alpha;
+          sshOpts = [ "-p" "14228" ];
+        };
+      };
+
+      # This is highly advised, and will prevent many possible mistakes
+      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
     };
 }
