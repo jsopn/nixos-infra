c79a755a91c29bf9ed5e5db8c318a26034cf73a1
jsopn <me@jsopn.com>
2024-08-22 05:27:45 +0400

    overlays: add sway and wlroots overlays
    fixes flickering on nvidia display

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,37 @@
 { inputs, ... }: {
   additions = final: _prev: import ../pkgs final.pkgs;
 
-  modifications = final: prev: { };
+  modifications = final: prev: {
+    sway-unwrapped = prev.sway-unwrapped.overrideAttrs (attrs: {
+      version = "0-unstable-2024-08-11";
+      src = final.fetchFromGitHub {
+        owner = "swaywm";
+        repo = "sway";
+        rev = "b44015578a3d53cdd9436850202d4405696c1f52";
+        hash = "sha256-gTsZWtvyEMMgR4vj7Ef+nb+wcXkwGivGfnhnBIfPHOA=";
+      };
+      buildInputs = attrs.buildInputs ++ [ final.wlroots ];
+      mesonFlags =
+        let
+          inherit (final.lib.strings) mesonEnable mesonOption;
+        in
+        [
+          (mesonOption "sd-bus-provider" "libsystemd")
+          (mesonEnable "tray" attrs.trayEnabled)
+        ];
+    });
+
+    wlroots = prev.wlroots.overrideAttrs (_attrs: {
+      version = "0-unstable-2024-08-11";
+      src = final.fetchFromGitLab {
+        domain = "gitlab.freedesktop.org";
+        owner = "wlroots";
+        repo = "wlroots";
+        rev = "6214144735b6b85fa1e191be3afe33d6bea0faee";
+        hash = "sha256-nuG2xXLDFsGh23CnhaTtdOshCBN/yILqKCSmqJ53vgI=";
+      };
+    });
+  };
 
   stable-packages = final: _prev: {
     stable = import inputs.nixpkgs-stable {
